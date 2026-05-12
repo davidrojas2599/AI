c8a8ee1191933bb1877eb8f9159e7e0dc139e231
David Rojas <david.rojas2599@gmail.com>
2026-05-12 10:55:21 -0700

    initial commit 3

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
+import requests
+
+def generate_x_post(usr_input: str) -> str:
+    payload = {
+        "model": "gpt-4.1-mini",
+        "input": usr_input
+    }
+    response = requests.post(
+        "https://api.openai.com/v1/responses",
+        json=payload,
+        headers={
+            "Content-Type": "application/json",
+            "Authorization": "Bearer adfssdf"
+        }
+    )
+    response.raise_for_status()
+    data = response.json()
+    return data.get("output", [{}])[0].get("content", [{}])[0].get("text", "")
+
+
 def main():
-    print("Hello from pythonai!")
+    # take user input => LLM to generate X post => output post
+
+    usr_input = input("what should the post be about? ")
+    x_post = generate_x_post(usr_input)
+    print("Generated X post")
+    print(x_post)
 
 
 if __name__ == "__main__":
